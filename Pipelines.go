a88b002450a6c0af98147867bf23d807920306c7
miraqe <annamaria.kalmus@gmail.com>
2023-07-14 17:08:16 +0300

    Added final error handling messages, cleaned up first part of the application, added comments to each part to explain what they mean

--- Pipelines.go
+++ Pipelines.go
@@ -8,19 +8,29 @@ import (
 )
 
 func getDeals() {
-	baseURL := "https://api.pipedrive.com/api/v1/deals?api_token=a9bfb11e27d8e6f01b7d0e8d88a53cda91c454ac"
+	// Set URL to connect to Pipedrive API with API token and parse the URL to format it properly
+	endpointURL, err := url.Parse("https://api.pipedrive.com/api/v1/deals?api_token=a9bfb11e27d8e6f01b7d0e8d88a53cda91c454ac")
 	method := "GET"
-
-	endpointUrl, _ := url.Parse(baseURL)
-
-	client := &http.Client{}
-	req, _ := http.NewRequest(method, endpointUrl.String(), nil)
-
-	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error parsing URL: ", err)
 		return
 	}
+
+	// Create HTTP client
+	client := &http.Client{}
+	// Create a new GET request to the endpoint URL
+	req, err := http.NewRequest(method, endpointURL.String(), nil)
+	if err != nil {
+		fmt.Println("Error creating a new GET request to the endpoint URL!")
+	}
+
+	// Send the request and get a response
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error getting a response from the server: ", err)
+		return
+	}
+	// Make sure response body is closed
 	defer func() {
 		err := res.Body.Close()
 		if err != nil {
@@ -28,9 +38,14 @@ func getDeals() {
 		}
 	}()
 
-	body, _ := io.ReadAll(res.Body)
-	println("Connection Successful! Showing all deals: ", string(body))
+	// Read the response body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error closing the response body: ", err)
+	}
 
+	// Print all deals that are found
+	println("Connection Successful! Showing all deals: ", string(body))
 }
 
 func main() {
