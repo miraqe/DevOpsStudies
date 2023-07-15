be770c8d3b2b6842608de662b2bfc553fd8a98c8
miraqe <annamaria.kalmus@gmail.com>
2023-07-15 12:52:34 +0300

    Added updateDeal function while using the most of the previous code, determined URL value to find the exact deal that needs changing

--- Pipelines.go
+++ Pipelines.go
@@ -110,7 +110,62 @@ func addDeal() {
 	println("Connection Successful! Deal added: ", string(body))
 }
 
+func updateDeal() {
+	// Set URL to connect to Pipedrive API with API token and parse the URL to format it properly, determine deal value
+	endpointUrl := "https://api.pipedrive.com/api/v1/deals/21?api_token=a9bfb11e27d8e6f01b7d0e8d88a53cda91c454ac"
+	method := "PUT"
+
+	// Change deal data
+	payloadData := map[string]interface{}{
+		"title":              "WinterBunches",
+		"value":              7654,
+		"currency":           "EUR",
+		"status":             "open",
+		"org_id":             4,
+		"participants_count": 1,
+	}
+
+	// Convert the payload data to JSON format
+	payloadBytes, _ := json.Marshal(payloadData)
+
+	// Create HTTP client
+	client := &http.Client{}
+
+	// Create a new POST request to the endpoint URL with the payload
+	req, err := http.NewRequest(method, endpointUrl, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		fmt.Println("Error creating a new GET request to the endpoint URL!")
+	}
+
+	// Set the content type to JSON
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the request and get a response
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+
+	// Ensure the response body is closed after reading
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			fmt.Println("Error closing response body:", err)
+		}
+	}()
+
+	// Read the response body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error closing the response body: ", err)
+	}
+	// Print the deal added
+	println("Connection successful! Deal has been changed: ", string(body))
+}
+
 func main() {
 	getDeals()
 	addDeal()
+	updateDeal()
 }
