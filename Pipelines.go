cb3a5913f17aeaa405e25c6dc9805183bdfcf894
miraqe <annamaria.kalmus@gmail.com>
2023-08-01 13:34:31 +0300

    Key rotation

--- Pipelines.go
+++ Pipelines.go
@@ -48,9 +48,18 @@ type Config struct {
 	PipedriveAPIToken string `json:"PipedriveAPIToken"`
 }
 
-// Add a function loadConfig, which loads the configuration key from the "config.json" file.
+// Add a function loadConfig, which loads the configuration key either from the environment variable or from the "config.json" file.
 func loadConfig() (Config, error) {
 	var config Config
+
+	// Check if the PIPEDRIVE_API_TOKEN environment variable is set
+	apiToken := os.Getenv("PIPEDRIVE_API_TOKEN")
+	if apiToken != "" {
+		config.PipedriveAPIToken = apiToken
+		return config, nil
+	}
+
+	// Read from "config.json" if the environment variable is not set
 	file, err := os.Open("config.json")
 	if err != nil {
 		return config, err
